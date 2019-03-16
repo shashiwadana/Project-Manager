b7bc1c3c8baef7289d02c0e7183482a38ccb84c5
zbuddy <shashiwadananirmani14@gmail.com>
2019-03-16 08:41:56 +0530

    basic config

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Project Manager
                 </div>
 
                 <div class="links">
