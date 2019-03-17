b19c27936f8c01342cb666e2c726fee8025352a0
zbuddy <shashiwadananirmani14@gmail.com>
2019-03-17 19:09:11 +0530

    basic tables

---
+++ database/migrations/2019_03_17_132821_create_tasks_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->integer('project_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->string('days')->unsigned();
+            $table->string('hours')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('project_id')->references('id')->on('projects');
+            $table->foreign('company_id')->references('id')->on('companies');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
