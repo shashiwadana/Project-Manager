79dd6d3c7792bc8987829bfb86cc4bfeba2bf720
zbuddy <shashiwadananirmani14@gmail.com>
2019-03-18 15:39:10 +0530

    user update

--- database/migrations/2019_03_17_132821_create_tasks_table.php
+++ database/migrations/2019_03_17_132821_create_tasks_table.php
@@ -18,8 +18,9 @@ class CreateTasksTable extends Migration
             $table->string('name');
             $table->integer('project_id')->unsigned();
             $table->integer('user_id')->unsigned();
-            $table->string('days')->unsigned();
-            $table->string('hours')->unsigned();
+            $table->string('days')->unsigned()->nullable();
+            $table->string('hours')->unsigned()->nullable();
+            $table->integer('company_id')->unsigned()->nullable();
             $table->foreign('user_id')->references('id')->on('users');
             $table->foreign('project_id')->references('id')->on('projects');
             $table->foreign('company_id')->references('id')->on('companies');
