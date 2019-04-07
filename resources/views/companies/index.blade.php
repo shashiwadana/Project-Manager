19d8e80a526dc57a728034cf09f63fd989cf4071
zbuddy <shashiwadananirmani14@gmail.com>
2019-04-07 19:52:22 +0530

    company view

---
+++ resources/views/companies/index.blade.php
@@ -0,0 +1,23 @@
+
+@extends('layouts.app ')
+
+@section('content')
+<div class="card">
+  <div class="card-header">
+    Quote
+  </div>
+  <div class="card-body">
+    
+<ul class="list-group">
+  <li class="list-group-item">Cras justo odio</li>
+  <li class="list-group-item">Dapibus ac facilisis in</li>
+  <li class="list-group-item">Morbi leo risus</li>
+  <li class="list-group-item">Porta ac consectetur ac</li>
+  <li class="list-group-item">Vestibulum at eros</li>
+</ul>
+
+    </blockquote>
+  </div>
+</div>
+
+@endsection
