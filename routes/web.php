01ff7f4cda831978fb1ac8097686916601c63630
zbuddy <shashiwadananirmani14@gmail.com>
2019-04-07 18:23:08 +0530

    routes n controllers

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::resource('companies', 'CompaniesController');
+Route::resource('projects', 'ProjectsController');
+Route::resource('roles', 'RolesController');
+Route::resource('tasks', 'TasksController');
+Route::resource('users', 'UsersController');
