cc5efb83272ab2121b70e2a478cdb97f0b2da886
zbuddy <shashiwadananirmani14@gmail.com>
2019-04-09 10:05:02 +0530

    company db

--- resources/views/companies/index.blade.php
+++ resources/views/companies/index.blade.php
@@ -2,22 +2,22 @@
 @extends('layouts.app ')
 
 @section('content')
-<div class="card">
-  <div class="card-header">
-    Quote
-  </div>
-  <div class="card-body">
-    
-<ul class="list-group">
-  <li class="list-group-item">Cras justo odio</li>
-  <li class="list-group-item">Dapibus ac facilisis in</li>
-  <li class="list-group-item">Morbi leo risus</li>
-  <li class="list-group-item">Porta ac consectetur ac</li>
-  <li class="list-group-item">Vestibulum at eros</li>
-</ul>
+<div class ="col-md-6 col-lg-6 col-md-offset-3 col-lg-offset-3">
+      <div class="card">
+        <div class="card-header">
+          Companies
+        </div>
+        <div class="card-body">
+          
+      <ul class="list-group">
+      @foreach($companies as $company)
+        <li class="list-group-item">{{$company->name}}</li>
+      @endforeach
+        
+      </ul>
 
-    </blockquote>
-  </div>
+
+        </div>
+      </div>
 </div>
-
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -72,9 +72,14 @@
             </div>
         </nav>
 
+
+       <div class="container">
         <main class="py-4">
             @yield('content')
         </main>
+       </div>
+       
+
     </div>
 </body>
 </html>
